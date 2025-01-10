6c0bffa8195f1bdca3fbce95d803e3972104b3fb
Srinath K K <srinathkk99@gmail.com>
2025-01-11 00:44:52 +0530

    initial commit

---
+++ src/data/resume.tsx
@@ -0,0 +1,283 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Srinath",
+  initials: "K K",
+  url: "https://dillion.io",
+  location: "Kannur, Kerala",
+  locationLink: "https://www.google.com/maps/place/kerala",
+  description:
+    "Penetration Tester & Bug Bounty Hunter. I love uncovering vulnerabilities and helping secure systems.",
+  summary:
+    "I am a Cybersecurity Analyst with a strong passion for ethical hacking and expertise in Vulnerability Assessment, Penetration Testing, and Bug Bounty Hunting. As a Certified Ethical Hacker, I specialize in identifying and securing vulnerabilities using tools like Burp Suite, Kali Linux, and Splunk. I have reported critical vulnerabilities to global organizations, including [NASA, Google, AWS, U.S. Department of Homeland Security](/#hackathons), and the [U.S. Department of Defense](/#hackathons), earning recognition in 20+ Hall of Fames. My dedication to cybersecurity has been acknowledged through awards and Letters of Recognition, reflecting my commitment to protecting digital assets and advancing [global security.](https://buildspace.so/sf1).",
+  avatarUrl: "/mee.jpg",
+  skills: [
+    "Penetration Testing",
+    "Vulnerability Assessment",
+    "Threat Intelligence",
+    "Bug Bounty Hunting",
+    "OWASP Top 10",
+    "Burp Suite",
+    "Metasploit",
+    "Network Security",
+    "Wireshark",
+    "Splunk",
+    "C++",
+    "SIEM",
+    "Nmap",
+    "Hydra",
+    "SQLMap",
+    "CSS",
+    "Python",
+    "HTML",
+    "C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "srinathkk99@gmail.com",
+    tel: "+123456789",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/spoilerOP",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/srinathkk",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "i",
+        url: "https://www.instagram.com/srinath.k.k",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://dub.sh/dillion-youtube",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Bugcrowd",
+      href: "https://www.bugcrowd.com",
+      badges: [],
+      location: "Remote",
+      title: "Security Researcher",
+      logoUrl: "https://logos.bugcrowdusercontent.com/logos/ef74/d1fa/62a5b64c/3809e0af42850a579f02c3434743e3ca_bugcrowd__1_.png",
+      start: "Sep 2024",
+      end: "Present",
+      description:
+        "Experience in conducting vulnerability assessments and reporting security flaws through bug bounty platforms such as Bugcrowd. Skilled in identifying and disclosing critical vulnerabilities to help organizations enhance their security posture.",
+    },
+    {
+      company: "TryHackMe",
+      badges: [],
+      href: "https://tryhackme.com",
+      location: "Remote",
+      title: "CTF Player",
+      logoUrl: "https://shoppylk.com/wp-content/uploads/2023/05/1_2lQXVuZ4cOaSdbWaf9NPYA-e1683802690631.png",
+      start: "Mar 2024",
+      end: "Present",
+      description:
+        "As a Capture The Flag (CTF) player on TryHackMe, I have consistently pursued excellence in cybersecurity. With a global ranking of 113131 (top 9%). This experience has allowed me to continuously enhance my skills in the dynamic field of cybersecurity, making me well-equipped to tackle various challenges. You can explore more about my CTF journey on my TryHackMe profile",
+    },
+    {
+      company: "Google Cloud Skills Boost",
+      href: "cloudskillsboost.google",
+      badges: [],
+      location: "Remote",
+      title: "Google Cloud Innovator",
+      logoUrl: "https://media.licdn.com/dms/image/v2/D560BAQFV-ds_iFfVSQ/company-logo_200_200/company-logo_200_200/0/1698660876286?e=2147483647&v=beta&t=GTNf3kD3LVtMJOG8AdTtdqv1aGmSDNC6zHOYBnEWyww",
+      start: "Mar 2024",
+      end: "Present",
+      description:
+        "The Google Cloud Arcade is a free, gamified learning platform where you solve hands-on labs in themed games to master cloud skills.",
+    },
+    {
+      company: "Freelance",
+      href: "https://li.me/",
+      badges: [],
+      location: "REMOTE",
+      title: "Graphic Designer",
+      logoUrl: "https://img.freepik.com/premium-vector/computer-display-with-illustration-design-program-concept_1005511-104.jpg?semt=ais_hybrid",
+      start: "Aug 2023",
+      end: "Present",
+      description:
+        "Freelance Graphic Designer with experience creating visually engaging designs for digital and print media. Skilled in delivering creative solutions tailored to client needs and ensuring brand consistency across projects.",
+    },
+    ,
+  ],
+  education: [
+    {
+      school: "BSOFT Education",
+      degree: "Diploma in Cyber Security",
+      logoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeesplyx5q0WLqFAtY7p25hDTSKy0WGyJrkA&s",
+      start: " 2024",
+      end: " 2025",
+    },
+    {
+      school: "Kannur University",
+      degree: "Bachelor's Degree of Computer Application (BCA)",
+      logoUrl: "https://career.justkerala.in/wp-content/uploads/2012/10/kannur-university-logo.jpg",
+      start: "2020",
+      end: "2023",
+    },
+    {
+      school: "Mambaram Higher Secondary School",
+      degree: "+2, Computer Science",
+      logoUrl: "https://www.static-contents.youth4work.com/university/Documents/Colleges/collegeLogo/16000.jpg?v=20161028144745",
+      start: "2018",
+      end: "2020",
+    },
+
+  ],
+  projects: [
+    {
+      title: "Lead Follower",
+      dates: "Nov 2022 - Mar 2023",
+      active: true,
+      description:
+        "*Lead Follower* is a real-time location tracking system for company employees, where team leaders create journeys, and members join via QR codes. It allows tracking of team members' locations through a map and detects accidents using sensors, ensuring safety and efficient task completion.",
+      technologies: [
+        "Python",
+        "Pycharm",
+        "SQLyog",
+        "WampServer",
+        "DreamWeaver",
+        "HTML",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image:
+       "",
+      video:
+        "leadv.mp4",
+    },
+    {
+      title: "Go Corona",
+      href: "https://srinathkk.itch.io/go-corona-2d/purchase",
+      dates: "April 2021",
+      active: true,
+      description:
+        "This game was developed during my first year of college using Unity, C#, and Photoshop. The objective is simple: avoid touching the viruses that appear on the screen. If you collide with a virus, you lose the game. The goal is to survive as long as possible by skillfully dodging the viruses. I designed the game mechanics in Unity and created the visual assets in Photoshop, focusing on a clean and engaging user experience.",
+      technologies: [
+        "c#",
+        "Unity3d",
+        "Gamedevelopment",
+        "Photoshop",
+        "VS Code",
+        "2D",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://srinathkk.itch.io/go-corona-2d/purchase",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "gocoronav.mp4",
+    },
+  ],
+  hackathons: [
+    {
+      title: "NASA",
+      dates: "December 25th 2024",
+      location: "Appreciation Letter & Hall of Fame",
+      description:
+        "Earned a prestigious Letter of Recommendation and Hall of Fame recognition from NASA for identifying and responsibly reporting a P4 vulnerability involving sensitive data exposure.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVpECSc-LbOLOpfLrqWHm5bSdkAjzBVQT2PQ&s",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+    {
+      title: "U.S. Department of Homeland Security",
+      dates: "December 16th 2024",
+      location: "Appreciation Letter & Hall of Fame",
+      description:
+        "Received a Letter of Recommendation and Hall of Fame recognition from the U.S. Department of Homeland Security (DHS) for identifying and responsibly reporting a P2 vulnerability.",
+      icon: "public",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Seal_of_the_U.S._Department_of_Homeland_Security.svg/293px-Seal_of_the_U.S._Department_of_Homeland_Security.svg.png",
+      links: [],
+    },
+    {
+      title: "NASA",
+      dates: "December 2nd 2024",
+      location: "Appreciation Letter & Hall of Fame",
+      description:
+        "Achieved my first appreciation and Hall of Fame recognition from NASA for discovering and responsibly reporting a P2 vulnerability that exposed usernames and passwords, highlighting my skills in uncovering critical security flaws.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVpECSc-LbOLOpfLrqWHm5bSdkAjzBVQT2PQ&s",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+
+    {
+      title: "Google",
+      dates: "November 12th, 2024",
+      location: "Awards",
+      description:
+        "Reported two valid P4 vulnerabilities to Google and earned two awards for my contributions, highlighting my ability to identify and responsibly disclose security issues in major platforms.",
+      image:
+        "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png",
+      links: [
+        {
+          title: "Profile",
+        //  icon: <Icons.github className="h-4 w-4" />,
+          href: "https://bughunters.google.com/profile/337352ad-a045-4c6a-bc77-85a3cc92f86a",
+        },
+      ],
+    },
+    {
+      title: "Meta",
+      dates: "July 19th, 2022",
+      location: "Competition",
+      description:
+        "Developed an innovative Augmented Reality (AR) effect that earned me a $300 prize in the Meta Spark competition. My work secured a spot in the top 500 globally, showcasing my creativity and technical skills in AR development on a global platform.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu_FHm6_xVJK229Dbw2rpd7b9cTStE0kQX8g&s",
+      links: [
+        {
+          title: "Post",
+         // icon: <Icons.github className="h-4 w-4" />,
+          href: "https://www.instagram.com/p/CgLz6xcBW_G/?igsh=NWQ3ZGJhb3diZGFn",
+        },
+        {
+          title: "ithil onnula",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Wallet6/my6footprint-machine-learning",
+        },
+      ],
+    },
+  ],
+} as const;
